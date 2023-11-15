5f615926d3158c73277fcb69f615d74c01a42204
MrKerim <muhammedenes.kaplan@std.yeditepe.edu.tr>
2023-11-15 12:28:28 +0300

    fixed bug

--- server/tasks/updateHomeworkStatus.js
+++ server/tasks/updateHomeworkStatus.js
@@ -3,16 +3,16 @@ const cron = require("node-cron");
 const connectDB = require("../utilities/connectDB");
 const Homework = require("../models/HomeworkModel");
 
-cron.schedule("0 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
   connectDB();
   const currentDate = new Date();
+  console.log("hitted");
 
   // expirationDate'i geçmiş olan ödevleri bul ve statüsünü güncelle
   const expiredHomeworks = await Homework.find({
     expirationDate: { $lt: currentDate },
     status: { $eq: "Pending" },
   });
-  console.log(expiredHomeworks);
 
   // Her bir ödevi "Failed" olarak güncelle
   for (const homework of expiredHomeworks) {
